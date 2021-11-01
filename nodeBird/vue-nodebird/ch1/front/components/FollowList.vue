94449c6f147c4ad260151aa2602262d5e88c45d3
newkayak12 <newkayak12@gmail.com>
2021-11-01 17:42:18 +0900

    vuetify

--- nodeBird/vue-nodebird/ch1/front/components/FollowList.vue
+++ nodeBird/vue-nodebird/ch1/front/components/FollowList.vue
@@ -1,5 +1,18 @@
 <template>
-
+     <v-list>
+          <v-list-item>
+               <span>제로초</span>
+               <v-icon>mdi-minus-circle-outline</v-icon>
+          </v-list-item>
+          <v-list-item>
+               <span>네로</span>
+               <v-icon>mdi-minus-circle-outline</v-icon>
+          </v-list-item>
+          <v-list-item>
+               <span>히어로</span>
+               <v-icon>mdi-minus-circle-outline</v-icon>
+          </v-list-item>
+     </v-list>
 </template>
 
 <script>
