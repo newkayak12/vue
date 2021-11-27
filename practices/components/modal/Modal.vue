3414c3916bdde9adc389d233566a3af804d66de9
newkayak12 <newkayak12@gmail.com>
2021-11-27 19:38:05 +0900

    YJ's Help

--- practices/components/modal/Modal.vue
+++ practices/components/modal/Modal.vue
@@ -1,25 +1,18 @@
 <template>
-     <div style="background-color: rgba(0,0,0,0.4); display: flex; justify-content: center; align-items: center;  width: 100vw;">
-               <v-card :style="widthHeight"  >
+     <div style="background-color: rgba(0,0,0,0.4); display: flex; justify-content: center; align-items;  width: 100vw;">
+               <v-card :style="widthHeight +' '+modalTop" style="position:fixed;" >
                     <div style=" width:100%; height: 100%;  display: flex; flex-direction: column; justify-content: space-between">
                          <div class="modalHeader" >
-                                   <div>
-                                        <div style="width: 38px; height: 38px;"></div>
-                                   </div>
-                                   <div>
-                                        <h4>
-                                             {{ followFlag==='follower'? '팔로워 _ [내가 팔로우한 사람들]':'팔로잉 _ [나를 팔로우한 사람들]' }}
-                                        </h4>
-                                   </div>
-                                   <div>
+                                        <h4>{{ followFlag==='follower'? '팔로워 _ [내가 팔로우한 사람들]':'팔로잉 _ [나를 팔로우한 사람들]' }}</h4>
                                         <v-icon style="color:royalblue; font-size: 38px" @click="closeModal">
                                              mdi-close-box
                                         </v-icon>
-                                   </div>
                          </div>
 
                          <div class="modalBody">
-                                   <FollowList v-for="followOne in follow"  :follow="followOne" :closeModal="closeModal" />
+
+                                   <FollowList v-for="followOne in follow"  :follow="followOne" :closeModal="closeModal" style="width:80%;"/>
+
                          </div>
 
 
@@ -63,15 +56,17 @@ export default {
      },
      computed:{
           widthHeight(){
-
-               return `width: ${document.documentElement.clientWidth*0.7-100}px; height: ${document.documentElement.clientHeight*0.7}px;`
+               return `width: ${document.documentElement.clientWidth*0.8}px; height: ${document.documentElement.clientHeight*0.7}px;`
+          },
+          modalTop(){
+               return `top: ${document.documentElement.clientHeight*0.1}px;`
           }
      }
 }
 </script>
 
 <style scoped>
-     .modalHeader,.modalFooter{
+     .modalFooter{
           width: 100%;
           height: 5%;
           display: flex;
@@ -79,6 +74,21 @@ export default {
           justify-content: space-between;
           align-items: center;
           border-bottom: 1px gray solid;
+     }
+     .modalHeader{
+          width: 100%;
+          height: 6%;
+          position: relative;
+          border-bottom: 1px gray solid;
+     }
+     .modalHeader h4 {
+          margin-top: 5px;
+          text-align: center;
+     }
+     .modalHeader button {
+          position: absolute;
+          top: 0;
+          right: 0;
      }
      .modalBody{
           width: 100%;
